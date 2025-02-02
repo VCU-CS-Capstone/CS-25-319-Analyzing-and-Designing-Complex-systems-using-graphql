0ccd061f001fed7a52e85db06027e22ef18993e5
JaysonU25 <jaysonurena035@gmail.com>
2025-02-02 11:03:00 -0500

    New Domain and Client ID

--- src/my-dashboard/src/App.js
+++ src/my-dashboard/src/App.js
@@ -14,7 +14,7 @@ import { UserRoleProvider } from './UserContext';
 
 // Auth0 configuration
 const domain = 'dev-aue4pbx1550zv4m3.us.auth0.com';
-const clientId = 'KANG9jMvm4IV5zaLlXTLBttcxrf0nlqE';
+const clientId = 'X2iJYUuZWuexN86VhBGxDSd0yl7iW8Nm';
 const audience = 'https://clean-wasp-50.hasura.app/v1/graphql'; // Hasura GraphQL endpoint
 const redirectUri = 'http://localhost:3000/';
 
